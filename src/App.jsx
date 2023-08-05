b35d37992180f6e45032318553f9f100e2c74257
Arindam Majumder <arindammajumder2020@gmail.com>
2023-08-05 15:30:15 +0530

    (feat):Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import IssuePage from "./pages/IssuePage";
 import LandingPage from "./pages/LandingPage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./Context/AuthContext";
 function App() {
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/list" element={<IssuePage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/SignupPage" element={<SignupPage />} />
+          <Route exact path="/LoginPage" element={<LoginPage />} />
+          <Route exact path="/list" element={<IssuePage />} />
+        </Routes>
+      </AuthProvider>
     </>
   );
 }
